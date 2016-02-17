ef3706e67e4415743cda8ebe4215e55bfcd0496e
jselfridge <jselfridge@gmail.com>
2016-02-17 16:05:44 +0000

    Revised syntax to match BlackBox code.

--- src/i2c.c
+++ src/i2c.c
@@ -6,11 +6,6 @@
 #include "i2c.h"
 
 
-//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-//  imu_init
-//  Initializes an MPU sensor.
-//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-
 /*
 #include <sys/time.h>
 #include <errno.h>
@@ -31,195 +26,228 @@
 
 //void __no_operation(void) { }
 
+
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+//  i2c_open
+//  Opens an I2C bus.
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 /*
-int i2c_open()
-{
-	char buff[32];
+int i2c_open (  )  {
 
-	if (!i2c_fd) {
-		sprintf(buff, "/dev/i2c-%d", i2c_bus);
+  char buff[32];
 
-#ifdef I2C_DEBUG
-		printf("\t\t\ti2c_open() : %s\n", buff);
-#endif
+  if (!i2c_fd) {
 
-		i2c_fd = open(buff, O_RDWR);
+    sprintf(buff, "/dev/i2c-%d", i2c_bus);
 
-		if (i2c_fd < 0) {
-			perror("open(i2c_bus)");
-			i2c_fd = 0;
-			return -1;
-		}
-	}
+    //#ifdef I2C_DEBUG
+    //printf("\t\t\ti2c_open() : %s\n", buff);
+    //#endif
 
-	return 0;
+    i2c_fd = open(buff, O_RDWR);
+
+    if ( i2c_fd < 0 ) {
+      perror("open(i2c_bus)");
+      i2c_fd = 0;
+      return -1;
+    }
+
+  }
+
+  return 0;
 }
 */
- /*
-void i2c_close()
-{
-	if (i2c_fd) {
-		close(i2c_fd);
-		i2c_fd = 0;
-		current_slave = 0;
-	}
+
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+//  i2c_close
+//  Closes an I2C bus.
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+/*
+void i2c_close (  )  {
+  if (i2c_fd) {
+    close(i2c_fd);
+    i2c_fd = 0;
+    current_slave = 0;
+  }
 }
- */
-  /*
-int i2c_select_slave(unsigned char slave_addr)
-{
-	if (current_slave == slave_addr)
-		return 0;
+*/
 
-	if (i2c_open())
-		return -1;
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+//  i2c_select_slave
+//  Assigns a slave address for an I2C bus.
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+/*
+int i2c_select_slave( unsigned char slave_addr )  {
+	
+  if (current_slave == slave_addr)
+    return 0;
 
-#ifdef I2C_DEBUG
-	printf("\t\ti2c_select_slave(%02X)\n", slave_addr);
-#endif
+  if ( i2c_open() )
+    return -1;
 
-	if (ioctl(i2c_fd, I2C_SLAVE, slave_addr) < 0) {
-		perror("ioctl(I2C_SLAVE)");
-		return -1;
-	}
+  //#ifdef I2C_DEBUG
+  //printf("\t\ti2c_select_slave(%02X)\n", slave_addr);
+  //#endif
 
-	current_slave = slave_addr;
+  if (ioctl(i2c_fd, I2C_SLAVE, slave_addr) < 0) {
+    //perror( "ioctl(I2C_SLAVE)" );
+    return -1;
+  }
 
-	return 0;
+  current_slave = slave_addr;
+
+  return 0;
 }
-  */
-   /*
-void linux_set_i2c_bus(int bus)
-{
-	if (i2c_fd)
-		i2c_close();
+*/
 
-	i2c_bus = bus;
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+//  i2c_set_bus
+//  Assigns the bus id.
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+/*
+void i2c_set_bus ( int bus )  {
+  if (i2c_fd)
+    i2c_close();
+
+  i2c_bus = bus;
 }
-   */
-    /*
-int linux_i2c_write(unsigned char slave_addr, unsigned char reg_addr,
-       unsigned char length, unsigned char const *data)
-{
-	int result, i;
+*/
 
-	if (length > MAX_WRITE_LEN) {
-		printf("Max write length exceeded in linux_i2c_write()\n");
-		return -1;
-	}
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+//  i2c_write
+//  Write data to the I2C bus.
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+/*
+int i2c_write ( unsigned char slave_addr, unsigned char reg_addr,
+       unsigned char length, unsigned char const *data )  {
 
-#ifdef I2C_DEBUG
-	printf("\tlinux_i2c_write(%02X, %02X, %u, [", slave_addr, reg_addr, length);
+  int result, i;
 
-	if (length == 0) {
-		printf(" NULL ] )\n");
-	}
-	else {
-		for (i = 0; i < length; i++)
-			printf(" %02X", data[i]); 
+  if (length > MAX_WRITE_LEN) {
+    printf("Max write length exceeded in linux_i2c_write()\n");
+    return -1;
+  }
 
-		printf(" ] )\n");
-	}
-#endif
+  #ifdef I2C_DEBUG
+  printf("\tlinux_i2c_write(%02X, %02X, %u, [", slave_addr, reg_addr, length);
+  if (length == 0) {
+    printf(" NULL ] )\n");
+  }
+  else {
+    for (i = 0; i < length; i++)
+      printf(" %02X", data[i]); 
 
-	if (i2c_select_slave(slave_addr))
-		return -1;
+    printf(" ] )\n");
+  }
+  #endif
 
-	if (length == 0) {
-		result = write(i2c_fd, &reg_addr, 1);
+  if (i2c_select_slave(slave_addr))
+    return -1;
 
-		if (result < 0) {
-			perror("write:1");
-			return result;
-		}
-		else if (result != 1) {
-			printf("Write fail:1 Tried 1 Wrote 0\n");
-			return -1;
-		}
-	}
-	else {
-		txBuff[0] = reg_addr;
+  if (length == 0) {
+    result = write(i2c_fd, &reg_addr, 1);
 
-		for (i = 0; i < length; i++)
-			txBuff[i+1] = data[i];
+    if (result < 0) {
+      perror("write:1");
+      return result;
+    }
+    else if (result != 1) {
+      printf("Write fail:1 Tried 1 Wrote 0\n");
+      return -1;
+    }
+  }
+  else {
+    txBuff[0] = reg_addr;
 
-		result = write(i2c_fd, txBuff, length + 1);
+    for (i = 0; i < length; i++)
+      txBuff[i+1] = data[i];
 
-		if (result < 0) {
-			perror("write:2");
-			return result;
-		}
-		else if (result < (int)length) {
-			printf("Write fail:2 Tried %u Wrote %d\n", length, result); 
-			return -1;
-		}
-	}
+    result = write(i2c_fd, txBuff, length + 1);
 
-	return 0;
+    if (result < 0) {
+      perror("write:2");
+      return result;
+    }
+    else if (result < (int)length) {
+      printf("Write fail:2 Tried %u Wrote %d\n", length, result); 
+      return -1;
+    }
+  }
+
+  return 0;
 }
-    */
-     /*
-int linux_i2c_read(unsigned char slave_addr, unsigned char reg_addr,
-       unsigned char length, unsigned char *data)
-{
-	int tries, result, total;
+*/
 
-#ifdef I2C_DEBUG
-	int i;
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+//  i2c_read
+//  Reads data from the I2C bus.
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+/*
+int i2c_read( unsigned char slave_addr, unsigned char reg_addr,
+       unsigned char length, unsigned char *data )  {
 
-	printf("\tlinux_i2c_read(%02X, %02X, %u, ...)\n", slave_addr, reg_addr, length);
-#endif
+  int tries, result, total;
 
-	if (linux_i2c_write(slave_addr, reg_addr, 0, NULL))
-		return -1;
+  //#ifdef I2C_DEBUG
+  //int i;
+  //printf("\tlinux_i2c_read(%02X, %02X, %u, ...)\n", slave_addr, reg_addr, length);
+  //#endif
 
-	total = 0;
-	tries = 0;
+  if (linux_i2c_write(slave_addr, reg_addr, 0, NULL))
+    return -1;
 
-	while (total < length && tries < 5) {
-		result = read(i2c_fd, data + total, length - total);
+  total = 0;
+  tries = 0;
 
-		if (result < 0) {
-			perror("read");
-			break;
-		}
+  while (total < length && tries < 5) {
+    result = read(i2c_fd, data + total, length - total);
 
-		total += result;
+    if (result < 0) {
+      perror("read");
+      break;
+    }
 
-		if (total == length)
-			break;
+    total += result;
 
-		tries++;		
-		linux_delay_ms(10);
-	}
+    if (total == length)
+      break;
 
-	if (total < length)
-		return -1;
+    tries++;		
+    linux_delay_ms(10);
+  }
 
-#ifdef I2C_DEBUG
-	printf("\tLeaving linux_i2c_read(), read %d bytes: ", total);
+  if (total < length)
+    return -1;
 
-	for (i = 0; i < total; i++)
-		printf("%02X ", data[i]); 
+  //#ifdef I2C_DEBUG
+  //printf("\tLeaving linux_i2c_read(), read %d bytes: ", total);
+  //for (i = 0; i < total; i++)
+  //  printf("%02X ", data[i]); 
+  //printf("\n");
+  //#endif
 
-	printf("\n");
-#endif
-
-	return 0;
+  return 0;
 }
-     */
-      /*
-int linux_delay_ms(unsigned long num_ms)
-{
-	struct timespec ts;
+*/
 
-	ts.tv_sec = num_ms / 1000;
-	ts.tv_nsec = (num_ms % 1000) * 1000000;
-
-	return nanosleep(&ts, NULL);
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+//  i2c_delay
+//  Add a short delay for proper timing.
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+/*
+int i2c_delay ( unsigned long num_ms )  {
+  struct timespec ts;
+  ts.tv_sec = num_ms / 1000;
+  ts.tv_nsec = (num_ms % 1000) * 1000000;
+  return nanosleep(&ts, NULL);
 }
-      */
-       /*
+*/
+
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+//  i2c_xxxx
+//  What the heck is this for????
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+/*
 int linux_get_ms(unsigned long *count)
 {
 	struct timeval t;
@@ -236,7 +264,7 @@ int linux_get_ms(unsigned long *count)
 
 	return 0;
 }
-       */
+*/
 
 
 
