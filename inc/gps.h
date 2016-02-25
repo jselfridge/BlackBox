f2aceca4e7515c3aed3cdb00bacd1d7422fe8ee7
jselfridge <jselfridge@gmail.com>
2016-02-25 00:11:58 +0000

    Able to configure GPS by sending <CR> and <LF> ASCII characters.

--- inc/gps.h
+++ inc/gps.h
@@ -9,20 +9,26 @@
 
 
 // Define other packets
-#define GPS_DEFAULT      "$PMTK104*37"
+//#define GPS_DEFAULT      "$PMTK104*37"
 
-// Define buad rate packets
-#define GPS_BAUD_57600  "$PMTK251,57600*2C"
-#define GPS_BAUD_9600   "$PMTK251,9600*17"
 
-// Define update rate packets
-#define GPS_UPDATE_100_MHZ  "$PMTK220,10000*2F"    // 10.0 sec
-#define GPS_UPDATE_200_MHZ  "$PMTK220,5000*1B"     //  5.0 sec
-#define GPS_UPDATE_001_HZ   "$PMTK220,1000*1F"     //  1.0 sec
-#define GPS_UPDATE_005_HZ   "$PMTK220,200*2C"      //  0.2 sec
-#define GPS_UPDATE_010_HZ   "$PMTK220,100*2F"      //  0.1 sec
+// Define GPS buad rates
+//#define GPS_BAUD_57600  "$PMTK251,57600*2C"
+//#define GPS_BAUD_9600   "$PMTK251,9600*17"
 
-#define GPS_RMCONLY         "$PMTK314,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0*29"
+
+// Define GPS update rates
+#define GPS_UPDATE_100_MHZ  "$PMTK220,10000*2F\r\n"    // 10.0 sec
+#define GPS_UPDATE_200_MHZ  "$PMTK220,5000*1B\r\n"     //  5.0 sec
+#define GPS_UPDATE_001_HZ   "$PMTK220,1000*1F\r\n"     //  1.0 sec
+#define GPS_UPDATE_005_HZ   "$PMTK220,200*2C\r\n"      //  0.2 sec
+#define GPS_UPDATE_010_HZ   "$PMTK220,100*2F\r\n"      //  0.1 sec
+
+
+// Define GPS data types
+#define GPS_RMCONLY         "$PMTK314,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0*29\r\n"
+#define GPS_GGAONLY         "$PMTK314,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0*29\r\n"
+#define GPS_RMCGGA          "$PMTK314,0,1,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0*28\r\n"
 
 
 // GPS functions
--- src/gps.c
+++ src/gps.c
@@ -12,15 +12,11 @@
 //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~   
 void gps_init ( void )  {
   if (DEBUG)  printf("Initializing GPS \n");
-
   int i;
-
   //i = write( uart1.fd, GPS_DEFAULT,       sizeof(GPS_DEFAULT)       );  usleep(i*200);  printf("i: %d \n",i);
-  //i = write( uart1.fd, GPS_BAUD_57600,    sizeof(GPS_BAUD_57600)    );  usleep(i*200);  printf("i: %d \n",i);
-  //i = write( uart1.fd, GPS_UPDATE_200_MHZ, sizeof(GPS_UPDATE_200_MHZ) );  usleep(i*200);  printf("i: %d \n",i);
-  i = write( uart1.fd, GPS_RMCONLY, sizeof(GPS_RMCONLY) );  usleep(i*200);  printf("i: %d \n",i);
-
-
+  //i = write( uart1.fd, GPS_BAUD_9600,     sizeof(GPS_BAUD_9600)     );  usleep(i*200);  printf("i: %d \n",i);
+  i = write( uart1.fd, GPS_GGAONLY,       sizeof(GPS_GGAONLY)        );  usleep(i*200);  printf("i: %d \n",i);
+  //i = write( uart1.fd, GPS_UPDATE_010_HZ, sizeof(GPS_UPDATE_010_HZ) );  usleep(i*200);  printf("i: %d \n",i);
   return;
 }
 
