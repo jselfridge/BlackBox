c53707d9179c2a3c13da29f1e21e0dfcc0c65d5f
jselfridge <jselfridge@gmail.com>
2016-07-27 12:33:36 +0000

    Developing SISO adaptive state feedback controller.

--- inc/adapt.h
+++ inc/adapt.h
@@ -5,9 +5,21 @@
 
 
 typedef struct adapt_struct {
+  double x1;
+  double x2;
+  double r;
+  double u;
   double kx1;
   double kx2;
   double kr;
+  double kp;
+  double kx1p;
+  double kx2p;
+  double krp;
+  double Gx1;
+  double Gx2;
+  double Gr;
+  double Gp;
   //pthread_mutex_t mutex;
 } adapt_struct;
 adapt_struct adapt_test;
--- src/adapt.c
+++ src/adapt.c
@@ -34,12 +34,62 @@ void adapt_exit ( void )  {
  */
 void adapt_update ( void )  {
 
-  //double x1, x2, u, r;
-  //double x1p, x2p, up, rp;
-  //double kx1, kx2, kr;
-  //double kx1p, kx2p, krp;
+  // Get current states (from measurements)
+  double x1 = 3.0;
+  double x2 = 0.4;
+  double r  = 3.3;
 
-  //u = kx1 * x1 + kx2 * x2 + kr * r;
+  // Get previous states (from structure)
+  double x1p = adapt_test.x1;
+  double x2p = adapt_test.x2;
+  double rp  = adapt_test.r;
+
+  // Get current state gains (from structure)
+  double kx1 = adapt_test.kx1;
+  double kx2 = adapt_test.kx2;
+  double kr  = adapt_test.kr;
+  double kp  = adapt_test.kp;
+
+  // Get previous state gains (from structure)
+  double kx1p = adapt_test.kx1p;
+  double kx2p = adapt_test.kx2p;
+  double krp  = adapt_test.krp;
+
+  // Get adaptive law gains
+  double Gx1 = adapt_test.Gx1;
+  double Gx2 = adapt_test.Gx2;
+  double Gr  = adapt_test.Gr;
+  double Gp  = adapt_test.Gp;
+
+  // Control input 
+  double u = kx1 * x1 + kx2 * x2 + kr * r;
+
+  // Auxilliary signals
+  double xi =  ( kx1 - kx1p ) * x1p  + ( kx2 - kx2p ) * x2p  + ( kr - krp ) * rp;
+  double eps = ( x1 - rp ) + ( kp * xi );  
+
+  // Normalizing signal
+  double m = 1 + x1p * x1p + x2p * x2p + rp * rp + xi * xi;
+  double n = eps / m;
+
+
+
+  // Adaptive laws
+  kx1 -= Gx1 * x1p * n;
+  kx2 -= Gx2 * x2p * n;
+  kr  -= Gr  * rp  * n;
+  kp  -= Gp  * xi  * n;
+
+  // Update states
+  adapt_test.x1 = x1;
+  adapt_test.x2 = x2;
+  adapt_test.r  = r;
+  adapt_test.u  = u;
+
+  // Update gains
+  adapt_test.kx1 = kx1;
+  adapt_test.kx2 = kx2;
+  adapt_test.kr  = kr;
 
   return;
 }
