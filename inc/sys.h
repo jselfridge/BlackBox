c6ed8f82983ba3b274c96f541cd956ea5a921815
jselfridge <jselfridge@gmail.com>
2016-08-09 16:46:17 +0000

    Flight testing and data logging new AHRS code.

--- inc/sys.h
+++ inc/sys.h
@@ -8,8 +8,8 @@
 #include <stdbool.h>
 
 
-#define DEBUG       true
-#define SYS_EXIT    false
+#define DEBUG       false
+#define SYS_EXIT    true
 #define SYS_IO      false
 #define SYS_IMU     false
 #define SYS_AHRS    false
